f599a45aaa1d255331244dcaa658b4490628ceb1
wouiSB <wouisb@is-sb.com>
2024-07-20 18:10:45 +0800

    [type] fix: set auto_detect_interface to true by default to prevent loop

--- type/route/__init__.py
+++ type/route/__init__.py
@@ -7,7 +7,7 @@ class Route(BaseModel):
     rules: list[Rule]
     rule_set: list[SerializeAsAny[Rule_set]]
     final: str = None
-    auto_detect_interface: bool = None
+    auto_detect_interface: bool = True
     override_android_vpn: bool = None
     default_interface: str = None
     default_mark: int = None
